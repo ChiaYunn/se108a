64ae9a5ddbf2c7a4b26aa5a0494b56d5d1cab40d
rolf0504 <fangrolf@gmail.com>
2019-10-02 14:08:54 +0800

    1

--- 01-test/rolflodash.js
+++ 01-test/rolflodash.js
@@ -4,46 +4,46 @@ const _ = module.exports = {}
 // _.chunk(['a', 'b', 'c', 'd'], 3) => [['a', 'b', 'c'], ['d']]
 _.chunk = function (list, n) {
   const clist = []
-  for (let i=0; i<list.length; i+=n) {
-    clist.push(list.slice(i, i+n))
+  for (let i = 0; i < list.length; i += n) {
+    clist.push(list.slice(i, i + n))
   }
   return clist
 }
 
-//_.drop([1, 2, 3]); => [2, 3]
-//_.drop([1, 2, 3], 2); => [3]
-//_.drop([1, 2, 3], 5); => []
-//_.drop([1, 2, 3], 0); => [1, 2, 3]
+// .drop([1, 2, 3]); => [2, 3]
+// .drop([1, 2, 3], 2); => [3]
+// .drop([1, 2, 3], 5); => []
+// .drop([1, 2, 3], 0); => [1, 2, 3]
 _.drop = function (list, n = 1) {
-  let rlist = [];
-  for (let i = n; i < list.length; i ++) {
-    rlist.push(list[i]);
+  const rlist = []
+  for (let i = n; i < list.length; i++) {
+    rlist.push(list[i])
   }
-  return rlist;
+  return rlist
 }
 
-//_.isNaN(NaN); => true
-//_.isNaN(new Number(NaN)); => true
-//isNaN(undefined); => true
-//_.isNaN(undefined); => false
+// _.isNaN(NaN); => true
+// _.isNaN(new Number(NaN)); => true
+// isNaN(undefined); => true
+// _.isNaN(undefined); => false
 _.isNaN = function (value) {
-  var stats = (value <= 0 || value >= 0 || value == undefined) ? false : true;
-  return stats;
+  var stats = (value <= 0 || value >= 0 || value === undefined) ? 0 : 1
+  return stats
 }
 
-//var array = [1, 2, 3];
-//_.reverse(array); => [3, 2, 1]
-//console.log(array); => [3, 2, 1]
+// var array = [1, 2, 3];
+// _.reverse(array); => [3, 2, 1]
+// console.log(array); => [3, 2, 1]
 _.reverse = function (array) {
-  array = array.reverse();
-  return array;
+  array = array.reverse()
+  return array
 }
 
-//_.initial([1, 2, 3]); => [1, 2]
+// _.initial([1, 2, 3]); => [1, 2]
 _.initial = function (array) {
-  let rarray = [];
-  for (let i = 0; i < array.length - 1; i ++) {
-    rarray.push(array[i]);
+  const rarray = []
+  for (let i = 0; i < array.length - 1; i++) {
+    rarray.push(array[i])
   }
-  return rarray;
+  return rarray
 }
--- 01-test/test/test.js
+++ 01-test/test/test.js
@@ -1,44 +1,47 @@
 const assert = require('assert')
 const _ = require('../rolflodash')
-var expect = require('chai').expect;
-
-describe('rolflodash', function() {
-  describe('chunk', function() {
-    it("_.chunk(['a', 'b', 'c', 'd'], 2) equalTo [ [ 'a', 'b' ], [ 'c', 'd' ] ]", function() {
-      assert.deepStrictEqual(_.chunk(['a', 'b', 'c', 'd'], 2), [ [ 'a', 'b'], ['c' , 'd' ] ])
-    });
-    it("_.chunk(['a', 'b', 'c', 'd'], 3) equalTo [ [ 'a', 'b', 'c' ], [ 'd' ] ]", function() {
-      assert.deepStrictEqual(_.chunk(['a', 'b', 'c', 'd'], 3), [ [ 'a', 'b', 'c' ], [ 'd' ] ])
-    });
-    it("_.chunk(['a', 'b', 'c', 'd'], 3) notEqualTo [ [ 'a', 'b'], ['c' , 'd' ] ]", function() {
-      assert.notDeepStrictEqual(_.chunk(['a', 'b', 'c', 'd'], 3), [ [ 'a', 'b'], ['c' , 'd' ] ])
-    });
-  });
-  describe('drop', function() {
-    it("_.drop([1, 2, 3]) equalTo [2, 3]", function() {
+var expect = require('chai').expect
+var describe
+describe('rolflodash', function () {
+  describe('chunk', function () {
+    var it
+    it("_.chunk(['a', 'b', 'c', 'd'], 2) equalTo [ [ 'a', 'b' ], [ 'c', 'd' ] ]", function () {
+      assert.deepStrictEqual(_.chunk(['a', 'b', 'c', 'd'], 2), [['a', 'b'], ['c', 'd']])
+    })
+    it("_.chunk(['a', 'b', 'c', 'd'], 3) equalTo [ [ 'a', 'b', 'c' ], [ 'd' ] ]", function () {
+      assert.deepStrictEqual(_.chunk(['a', 'b', 'c', 'd'], 3), [['a', 'b', 'c'], ['d']])
+    })
+    it("_.chunk(['a', 'b', 'c', 'd'], 3) notEqualTo [ [ 'a', 'b'], ['c' , 'd' ] ]", function () {
+      assert.notDeepStrictEqual(_.chunk(['a', 'b', 'c', 'd'], 3), [['a', 'b'], ['c', 'd']])
+    })
+  })
+  describe('drop', function () {
+    var it
+    it('_.drop([1, 2, 3]) equalTo [2, 3]', function () {
       assert.deepStrictEqual(_.drop([1, 2, 3]), [2, 3])
-    });
-    it("_.drop([1, 2, 3], 2) equalTo [3]", function() {
+    })
+    it('_.drop([1, 2, 3], 2) equalTo [3]', function () {
       assert.deepStrictEqual(_.drop([1, 2, 3], 2), [3])
-    });
-    it("_.drop([1, 2, 3], 5) equalTo []", function() {
+    })
+    it('_.drop([1, 2, 3], 5) equalTo []', function () {
       assert.deepStrictEqual(_.drop([1, 2, 3], 5), [])
-    });
-    it("_.drop([1, 2, 3], 0) equalTo [1, 2, 3]", function() {
+    })
+    it('_.drop([1, 2, 3], 0) equalTo [1, 2, 3]', function () {
       assert.deepStrictEqual(_.drop([1, 2, 3], 0), [1, 2, 3])
-    });
-  });
-  describe('isNaN', function() {
-    it("_.isNaN(NaN) equalTo true", function() {
+    })
+  })
+  describe('isNaN', function () {
+    var it
+    it('_.isNaN(NaN) equalTo true', function () {
       assert.deepStrictEqual(_.isNaN(NaN), true)
-    });
-    it("_.isNaN(new Number(NaN)) equalTo true", function() {
-      assert.deepStrictEqual(_.isNaN(new Number(NaN)), true)
-    });
-    it("isNaN(undefined) equalTo true", function() {
+    })
+    it('_.isNaN(new Number(NaN)) equalTo true', function () {
+      assert.deepStrictEqual(_.isNaN(new Number (NaN)), true)
+    })
+    it("isNaN(undefined) equalTo true", function () {
       assert.deepStrictEqual(isNaN(undefined), true)
-    });
-    it("_.isNaN(undefined) equalTo false", function() {
+    })
+    it("_.isNaN(undefined) equalTo false", function () {
       assert.deepStrictEqual(_.isNaN(undefined), false)
     });
   });
