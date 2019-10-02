59d0a333bb76068146745ba9a64aa7ac0499775a
rolf0504 <fangrolf@gmail.com>
2019-10-02 14:34:14 +0800

    1

--- 01-test/test/test.js
+++ 01-test/test/test.js
@@ -6,13 +6,22 @@ describe('rolflodash', function () {
   describe('chunk', function () {
     var it
     it("_.chunk(['a', 'b', 'c', 'd'], 2) equalTo [ [ 'a', 'b' ], [ 'c', 'd' ] ]", function () {
-      assert.deepStrictEqual(_.chunk(['a', 'b', 'c', 'd'], 2), [['a', 'b'], ['c', 'd']])
+      assert.deepStrictEqual(_.chunk(['a', 'b', 'c', 'd'], 2), [
+        ['a', 'b'],
+        ['c', 'd']
+      ])
     })
     it("_.chunk(['a', 'b', 'c', 'd'], 3) equalTo [ [ 'a', 'b', 'c' ], [ 'd' ] ]", function () {
-      assert.deepStrictEqual(_.chunk(['a', 'b', 'c', 'd'], 3), [['a', 'b', 'c'], ['d']])
+      assert.deepStrictEqual(_.chunk(['a', 'b', 'c', 'd'], 3), [
+        ['a', 'b', 'c'],
+        ['d']
+      ])
     })
     it("_.chunk(['a', 'b', 'c', 'd'], 3) notEqualTo [ [ 'a', 'b'], ['c' , 'd' ] ]", function () {
-      assert.notDeepStrictEqual(_.chunk(['a', 'b', 'c', 'd'], 3), [['a', 'b'], ['c', 'd']])
+      assert.notDeepStrictEqual(_.chunk(['a', 'b', 'c', 'd'], 3), [
+        ['a', 'b'],
+        ['c', 'd']
+      ])
     })
   })
   describe('drop', function () {
@@ -36,28 +45,30 @@ describe('rolflodash', function () {
       assert.deepStrictEqual(_.isNaN(NaN), true)
     })
     it('_.isNaN(new Number(NaN)) equalTo true', function () {
-      assert.deepStrictEqual(_.isNaN(new Number (NaN)), true)
+      assert.deepStrictEqual(_.isNaN(new Number(NaN)), true)
     })
-    it("isNaN(undefined) equalTo true", function () {
+    it('isNaN(undefined) equalTo true', function () {
       assert.deepStrictEqual(isNaN(undefined), true)
     })
-    it("_.isNaN(undefined) equalTo false", function () {
+    it('_.isNaN(undefined) equalTo false', function () {
       assert.deepStrictEqual(_.isNaN(undefined), false)
-    });
-  });
-  describe('reverse', function() {
-    var array = [1, 2, 3];
-    it("_.reverse(array) equalTo [3, 2, 1]", function() {
+    })
+  })
+  describe('reverse', function () {
+    var it
+    var array = [1, 2, 3]
+    it('_.reverse(array) equalTo [3, 2, 1]', function () {
       assert.deepStrictEqual(_.reverse(array), [3, 2, 1])
-    });
-    it("array after _.reverse(array) equalTo [3, 2, 1]", function() {
+    })
+    it('array after _.reverse(array) equalTo [3, 2, 1]', function () {
       assert.deepStrictEqual(array, [3, 2, 1])
-    });
-  });
-  describe('initial', function() {
-    it("_.initial([1, 2, 3]) equalTo [1, 2]", function() {
-      expect(_.initial([1, 2, 3])).to.be.a("array");
-      expect(_.initial([1, 2, 3])).to.eql([1, 2]);
-    });
-  });
-});
+    })
+  })
+  describe('initial', function () {
+    var it
+    it('_.initial([1, 2, 3]) equalTo [1, 2]', function () {
+      expect(_.initial([1, 2, 3])).to.be.a('array')
+      expect(_.initial([1, 2, 3])).to.eql([1, 2])
+    })
+  })
+})
