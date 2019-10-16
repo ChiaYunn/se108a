351e0c133618a1daa5d0b4f3908ac56061e7a68f
rolf0504 <fangrolf@gmail.com>
2019-10-16 16:39:03 +0800

    1

--- 03-recTab/04-noRecurA.js
+++ 03-recTab/04-noRecurA.js
@@ -1,12 +1,12 @@
 
-console.log('fibonacci(43)=', fibonacci(5))
+console.log('fibonacci(43)=', fibonacci(43))
 
 function fibonacci(n) {
     if (n === 0)
         return 0
     if (n === 1)
         return 1
-        
+
     var ft = [0, 1]
 
     for(var i = 0; i < n - 1; i ++){
